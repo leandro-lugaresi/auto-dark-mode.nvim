f4eddc85a504b9febe8625801c724f3a806d17c6
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2024-05-24 22:06:36 -0300

    fix: check for apple command not working when the shell have some output
    This was happening for example when using fnm and node packages.
    The fnm integration will print the node version been used,
    breaking the check.

--- lua/auto-dark-mode/init.lua
+++ lua/auto-dark-mode/init.lua
@@ -22,94 +22,93 @@ local system
 ---@param res string
 ---@return boolean
 local function parse_query_response(res)
-	if system == "Linux" then
-		-- https://github.com/flatpak/xdg-desktop-portal/blob/c0f0eb103effdcf3701a1bf53f12fe953fbf0b75/data/org.freedesktop.impl.portal.Settings.xml#L32-L46
-		-- 0: no preference
-		-- 1: dark
-		-- 2: light
-		return string.match(res, "uint32 1") ~= nil
-	elseif system == "Darwin" then
-		return res == "Dark"
-	elseif system == "Windows_NT" or system == "WSL" then
-		-- AppsUseLightTheme    REG_DWORD    0x0 : dark
-		-- AppsUseLightTheme    REG_DWORD    0x1 : light
-		return string.match(res, "1") == nil
-	end
-	return false
+  if system == "Linux" then
+    -- https://github.com/flatpak/xdg-desktop-portal/blob/c0f0eb103effdcf3701a1bf53f12fe953fbf0b75/data/org.freedesktop.impl.portal.Settings.xml#L32-L46
+    -- 0: no preference
+    -- 1: dark
+    -- 2: light
+    return string.match(res, "uint32 1") ~= nil
+  elseif system == "Darwin" then
+    return string.match(res, "Dark") ~= nil
+  elseif system == "Windows_NT" or system == "WSL" then
+    -- AppsUseLightTheme    REG_DWORD    0x0 : dark
+    -- AppsUseLightTheme    REG_DWORD    0x1 : light
+    return string.match(res, "1") == nil
+  end
+  return false
 end
 
 ---@param callback fun(is_dark_mode: boolean)
 local function check_is_dark_mode(callback)
-	utils.start_job(query_command, {
-		on_stdout = function(data)
-			-- we only care about the first line of the response
-			local is_dark_mode = parse_query_response(data[1])
-			callback(is_dark_mode)
-		end,
-	})
+  utils.start_job(query_command, {
+    on_stdout = function(data)
+      local is_dark_mode = parse_query_response(data)
+      callback(is_dark_mode)
+    end,
+  })
 end
 
 ---@param is_dark_mode boolean
 local function change_theme_if_needed(is_dark_mode)
-	if is_dark_mode == is_currently_dark_mode then
-		return
-	end
+  if is_dark_mode == is_currently_dark_mode then
+    return
+  end
 
-	is_currently_dark_mode = is_dark_mode
-	if is_currently_dark_mode then
-		set_dark_mode()
-	else
-		set_light_mode()
-	end
+  is_currently_dark_mode = is_dark_mode
+  if is_currently_dark_mode then
+    set_dark_mode()
+  else
+    set_light_mode()
+  end
 end
 
 local function start_check_timer()
-	timer_id = vim.fn.timer_start(update_interval, function()
-		check_is_dark_mode(change_theme_if_needed)
-	end, { ["repeat"] = -1 })
+  timer_id = vim.fn.timer_start(update_interval, function()
+    check_is_dark_mode(change_theme_if_needed)
+  end, { ["repeat"] = -1 })
 end
 
 local function init()
-	if string.match(vim.loop.os_uname().release, "WSL") then
-		system = "WSL"
-	else
-		system = vim.loop.os_uname().sysname
-	end
+  if string.match(vim.loop.os_uname().release, "WSL") then
+    system = "WSL"
+  else
+    system = vim.loop.os_uname().sysname
+  end
 
-	if system == "Darwin" then
-		query_command = "defaults read -g AppleInterfaceStyle"
-	elseif system == "Linux" then
-		if not vim.fn.executable("dbus-send") then
-			error([[
+  if system == "Darwin" then
+    query_command = "defaults read -g AppleInterfaceStyle"
+  elseif system == "Linux" then
+    if not vim.fn.executable("dbus-send") then
+      error([[
 		`dbus-send` is not available. The Linux implementation of
 		auto-dark-mode.nvim relies on `dbus-send` being on the `$PATH`.
 	  ]])
-		end
+    end
 
-		query_command = table.concat({
-			"dbus-send --session --print-reply=literal --reply-timeout=1000",
-			"--dest=org.freedesktop.portal.Desktop",
-			"/org/freedesktop/portal/desktop",
-			"org.freedesktop.portal.Settings.Read",
-			"string:'org.freedesktop.appearance'",
-			"string:'color-scheme'",
-		}, " ")
-	elseif system == "Windows_NT" or system == "WSL" then
-		-- Don't swap the quotes; it breaks the code
-		query_command =
-			'reg.exe Query "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize" /v AppsUseLightTheme | findstr.exe "AppsUseLightTheme"'
-	else
-		return
-	end
+    query_command = table.concat({
+      "dbus-send --session --print-reply=literal --reply-timeout=1000",
+      "--dest=org.freedesktop.portal.Desktop",
+      "/org/freedesktop/portal/desktop",
+      "org.freedesktop.portal.Settings.Read",
+      "string:'org.freedesktop.appearance'",
+      "string:'color-scheme'",
+    }, " ")
+  elseif system == "Windows_NT" or system == "WSL" then
+    -- Don't swap the quotes; it breaks the code
+    query_command =
+      'reg.exe Query "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize" /v AppsUseLightTheme | findstr.exe "AppsUseLightTheme"'
+  else
+    return
+  end
 
-	if vim.fn.has("unix") ~= 0 then
-		if vim.loop.getuid() == 0 then
-			query_command = "su - $SUDO_USER -c " .. query_command
-		end
-	end
+  if vim.fn.has("unix") ~= 0 then
+    if vim.loop.getuid() == 0 then
+      query_command = "su - $SUDO_USER -c " .. query_command
+    end
+  end
 
-	if type(set_dark_mode) ~= "function" or type(set_light_mode) ~= "function" then
-		error([[
+  if type(set_dark_mode) ~= "function" or type(set_light_mode) ~= "function" then
+    error([[
 
 		Call `setup` first:
 
@@ -123,34 +122,34 @@ local function init()
 			end,
 		})
 		]])
-	end
+  end
 
-	check_is_dark_mode(change_theme_if_needed)
-	start_check_timer()
+  check_is_dark_mode(change_theme_if_needed)
+  start_check_timer()
 end
 
 local function disable()
-	vim.fn.timer_stop(timer_id)
+  vim.fn.timer_stop(timer_id)
 end
 
 ---@param options AutoDarkModeOptions
 local function setup(options)
-	options = options or {}
+  options = options or {}
 
-	---@param background string
-	local function set_background(background)
-		vim.api.nvim_set_option_value("background", background, {})
-	end
+  ---@param background string
+  local function set_background(background)
+    vim.api.nvim_set_option_value("background", background, {})
+  end
 
-	set_dark_mode = options.set_dark_mode or function()
-		set_background("dark")
-	end
-	set_light_mode = options.set_light_mode or function()
-		set_background("light")
-	end
-	update_interval = options.update_interval or 3000
+  set_dark_mode = options.set_dark_mode or function()
+    set_background("dark")
+  end
+  set_light_mode = options.set_light_mode or function()
+    set_background("light")
+  end
+  update_interval = options.update_interval or 3000
 
-	init()
+  init()
 end
 
 return { setup = setup, init = init, disable = disable }
--- lua/auto-dark-mode/utils.lua
+++ lua/auto-dark-mode/utils.lua
@@ -1,30 +1,33 @@
 local M = {}
 
 ---@param cmd string
----@param opts {input?: string, on_stdout?: function, on_exit?: function}
+---@param opts {input?: string, oa_stdout?: function, on_exit?: function}
 ---@return number | 'the job id'
 function M.start_job(cmd, opts)
-	opts = opts or {}
-	local id = vim.fn.jobstart(cmd, {
-		stdout_buffered = true,
-		on_stdout = function(_, data, _)
-			if data and opts.on_stdout then
-				opts.on_stdout(data)
-			end
-		end,
-		on_exit = function(_, data, _)
-			if opts.on_exit then
-				opts.on_exit(data)
-			end
-		end,
-	})
+  opts = opts or {}
+  local id = vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    on_stdout = function(_, data, _)
+      if type(data) == "table" then
+        data = table.concat(data, "\n")
+      end
+      if data and opts.on_stdout then
+        opts.on_stdout(data)
+      end
+    end,
+    on_exit = function(_, data, _)
+      if opts.on_exit then
+        opts.on_exit(data)
+      end
+    end,
+  })
 
-	if opts.input then
-		vim.fn.chansend(id, opts.input)
-		vim.fn.chanclose(id, "stdin")
-	end
+  if opts.input then
+    vim.fn.chansend(id, opts.input)
+    vim.fn.chanclose(id, "stdin")
+  end
 
-	return id
+  return id
 end
 
 return M
